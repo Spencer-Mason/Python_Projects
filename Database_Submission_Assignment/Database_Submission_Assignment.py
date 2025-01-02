98b3ca13036bf29cb67cd282843a0200b1f61e34
Spencer Mason <spencer.d.mason19@gmail.com>
2025-01-02 15:04:18 -0800

    Added comments

--- Database_Submission_Assignment/Database_Submission_Assignment.py
+++ Database_Submission_Assignment/Database_Submission_Assignment.py
@@ -1,7 +1,10 @@
 import sqlite3
 
+#Creates a variable that is a shortcut to linking to the database
+#And creates the database file if it doesn't exist
 conn = sqlite3.connect('test2.db')
 
+#Links to the database and creates a table
 with conn:
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_files( \
@@ -13,6 +16,7 @@ with conn:
 fileList = ('information.docx', 'Hello.txt', 'myImage.png', \
             'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
 
+#Loops through the file list to find files that end with the '.txt' extention
 for file in fileList:
     if file.endswith('.txt'):
         with conn:
